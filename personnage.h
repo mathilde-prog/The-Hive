9b04ecf483e1023759821b06a7f1cc0b2ee0b351
mathilde-prog <60465881+mathilde-prog@users.noreply.github.com>
2020-01-30 14:42:11 +0100

    Add files via upload
    Fichiers contenant les structures et énumérations nécessaires pour la création des personnages

---
+++ personnage.h
@@ -0,0 +1,36 @@
+
+typedef enum {none,sprinter,metabolism,luck,scout} skill_t; /* possibilité compétence spéciale */
+
+typedef enum {armor,weapon,misc,food} item_t; /* possibilité item */
+
+typedef enum {inconnu,bandit,maraudeur,animal,paysan, }
+
+typedef struct obj_s{
+  char nom[20];
+  item_t type;
+  int price; // valeur objet
+
+  int attack; // valeur attaque
+  int defense; // valeur défense
+  int equipped; // indique si le joueur est équipé de cet objet
+} obj_t;
+
+typedef struct perso_s {
+  int pv; // Points de vie
+  int pe; // Points d'énergie
+  int pa; // Points d'action
+  int karma; // Karma
+
+
+  skill_t competence; // Compétence spéciale
+  int nb_weapons;  // number of weapons : 0, 1 ou 2
+  int armor; // armor ? 0 ou 1
+
+} perso_t;
+
+typedef struct npc_s{
+
+
+
+
+} npc_t;
