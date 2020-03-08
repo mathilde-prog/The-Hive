0ee6e9ea8d06366067f43bb8719315f3318335bb
Mathilde <Mathilde.Mottay.Etu@univ-lemans.fr>
2020-03-08 14:56:59 +0100

    items.txt et structure.h mis à jour

--- structure.h
+++ structure.h
@@ -1,17 +1,16 @@
 // structure.h
 
 /************************************* MAP *************************************/
-#define NB_CATEG 3
-#define ITEMS_MAX 5
-#define D 15
+#define ITEMS_MAX 5 // maximum number of items retrievable on a hexagon of the map
+#define D 15 				// dimensions of the map
 
 typedef enum {prairie = 1, foret, ville, lac, camp_mil, camp_ban, market, favella, montagne, frontiere, mer, wasteland} hex_t;
-typedef enum {nature, urbain, militaire} categ_hexa;
+typedef enum {other, nature, urbain, militaire} categ_hexa;
+// + other : camp_ban + frontiere
 
-int coordonnees_valides (int x, int y);
-void clrscr(); // fonction pour clear l'output de terminal
-int range(int a,int b); // generates random number in range
-int rng(int prob); // returns 0 or 1 with a given chance
+void clrscr();
+int range(int a,int b);
+int rng(int prob);
 void init_border(int map[D][D]);
 int bordercross(int i, int j, int map[D][D]);
 void topup(int map[D][D]);
@@ -23,7 +22,8 @@ void init_base(int map[D][D]);
 void count(const int map[D][D]);
 void display_grid(const int map[D][D]);
 void look_around(int i, int j, int map[D][D]);
-void map_generation(int map[D][D]);
+void map_init(int map[D][D]);
+
 /*********************************************************************************/
 
 /************************************* ITEMS *************************************/
@@ -36,7 +36,6 @@ typedef struct item_s{
  	int attack; 				// value of attack
 	int defense; 				// value of defense
 	equip_t equipable; 	// indicates whether the player can equip himself with this item and where
-
 	int pc_nature; 			// Percentage chance of finding this item in a nature hexagon
 	int pc_urban;  			// Percentage chance of finding this item in an urban hexagon
 	int pc_military;		// Percentage chance of finding this item in a military hexagon
@@ -45,14 +44,13 @@ typedef struct item_s{
 int creation_tab_item(item_t * Tab_Items, int * nb_items);
 void display_item (item_t item);
 int nb_items_hexagone (int x, int y, int map[D][D], item_t * Tab_Items, int nb_items);
-void items_generer(item_t * Tab_Items, int nb_items, categ_hexa categ);
-void tests_item_generation(int map[D][D], item_t * Tab_Items, int nb_items_available); // Fonction pour Anaïs
+//void tests_item_generation(int map[D][D], item_t * Tab_Items, int nb_items_available); // Fonction pour Anaïs
 /*********************************************************************************/
 
 /************************************* PERSO *************************************/
 #define INVENTORY_CAPACITY 10 /* 10 items version 1 */
 
-typedef enum {normal,sprinter,metabolism,luck,scout} skill_t; /* possibilité compétence spéciale - version1 */
+typedef enum {normal,sprinter,metabolism,luck,scout} skill_t;
 
 typedef struct perso_s {
   int pv; // Points de vie
@@ -60,8 +58,9 @@ typedef struct perso_s {
   int pa; // Points d'action
 	int posX;
 	int posY;
+	int turns; //nb tours
 
-  skill_t competence; // Compétence spéciale - dans la version 1 ?
+  skill_t competence; // Compétence spéciale
 
   item_t inventory[INVENTORY_CAPACITY];
   int nb_items_inventory;
@@ -73,8 +72,9 @@ typedef struct perso_s {
 
 } perso_t;
 
-void display_player_characteristics(perso_t player);
 void init_player(perso_t * player);
+void display_player_characteristics(int map[D][D], perso_t player);
+
 /**********************************************************************************/
 
 /*********************************** INVENTORY ***********************************/
@@ -82,7 +82,8 @@ int indice(perso_t player, item_t item);
 int too_much_of_the_same_item(perso_t player, item_t item);
 void display_inventory (perso_t player);
 void delete_item_in_inventory(perso_t * player, item_t item);
-void add_item_to_inventory(perso_t * player, item_t item);
+int add_item_to_inventory(perso_t * player, item_t item);
+void manage_inventory(perso_t * player); 
 /*********************************************************************************/
 
 /*********************************** EQUIPMENT ***********************************/
@@ -97,8 +98,32 @@ int is_equipped(perso_t player, item_t item);
 void swap_equipment_player(perso_t * player, item_t item);
 void equip_player(perso_t * player);
 void remove_equipment_player(perso_t * player);
+void manage_equipment(perso_t * player);
 /**********************************************************************************/
 
-/*********************************** EAT ***********************************/
+/*********************************** EAT_OR_DRINK ***********************************/
 void eat_or_drink (perso_t * player, item_t item);
+/************************************************************************************/
+
+/*********************************** SCAVENGE ***********************************/
+categ_hexa category_hexagon (int map[D][D], int x, int y);
+void generate_items(item_t * Tab_Items, int nb_items_available, perso_t * player, categ_hexa categ);
+void scavenge(int map[D][D], perso_t * player, item_t * Tab_Items, int nb_items_available);
+/************************************************************************************/
+
+/*********************************** BACKUP AND LOAD ***********************************/
+void save (perso_t player, int map[D][D]);
+void save_inventory (perso_t player);
+void save_info_player (perso_t player);
+void save_equipment (perso_t player);
+void save_map (int map[D][D]);
+
+int load (perso_t * player, int map[D][D]);
+int load_inventory (perso_t * player);
+int load_info_player (perso_t * player);
+int load_equipment (perso_t * player);
+int load_map(int map[D][D]);
+
+int backup_exists ();
+int init_or_load_game (perso_t * player, int map[D][D]);
 /***************************************************************************/
