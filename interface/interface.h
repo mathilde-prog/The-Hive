f298e4d250a482f309f197ccaf4ae048230bfd97
hodelll <clem72.mainguy@gmail.com>
2020-03-19 11:13:49 +0100

    update .h
    adding functions wrotted in .c

--- interface/interface.h
+++ interface/interface.h
@@ -1,4 +1,5 @@
 #define N 14
+void init_map_essai(int mapint[N][N]);
 void relation_hexa_char(char*mapchar[], int mapint[][N]);
 void affichage_map(SDL_Renderer **renderer, char *map[]);
 int interface();
