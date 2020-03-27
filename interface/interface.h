a07c2e98eff62822a314d36c1488f7d015862cb6
hodelll <clem72.mainguy@gmail.com>
2020-03-27 14:34:51 +0100

    maj .h

--- interface/interface.h
+++ interface/interface.h
@@ -1,5 +1,6 @@
 #define N 14
 void init_map_essai(int mapint[N][N]);
 void relation_hexa_char(char*mapchar[], int mapint[][N]);
+void affichage_case_centrale(SDL_Renderer **renderer);
 void affichage_map(SDL_Renderer **renderer, char *map[]);
 int interface();
